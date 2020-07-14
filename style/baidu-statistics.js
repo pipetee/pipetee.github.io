85096b9f9c47573d66d536f428097d352ab85a13
min.yang <min.yang@beibei.com>
2020-07-14 14:34:56 +0800

    init

---
+++ style/baidu-statistics.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?60399d7431fdd2695c08d3c2a05fe972";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
